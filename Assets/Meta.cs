52cf46c7357d60f03e8c9cd18bc248a1daca5561
angel clase <aroldanrabanal@safareyes.es>
2026-02-17 10:32:57 +0100

    cambios en la meta y Nivel2

--- Assets/Meta.cs
+++ Assets/Meta.cs
@@ -7,8 +7,16 @@ public class Meta : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            // Carga la siguiente escena por su nombre
-            SceneManager.LoadScene("Nivel2"); 
+            int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+            
+            if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+            {
+                SceneManager.LoadScene(nextSceneIndex);
+            }
+            else
+            {
+                Debug.Log("¡Felicidades! Has llegado al final del juego.");
+            }
         }
     }
 }
